5c04b4846b739efd4fd4fafc50aa9ad63707f77a
Luiz Barros <lck.barros@gmail.com>
2025-06-05 19:49:26 -0300

    Starting the Challenge - Crud

---
+++ src/main/java/com/luizbarros/crud/CrudApplication.java
@@ -0,0 +1,18 @@
+package com.luizbarros.crud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server ON!");
+	}
+}
