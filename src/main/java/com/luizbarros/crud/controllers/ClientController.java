9887c0af6f607267bd5345e1cf3d5e6c149d9509
Luiz Barros <lck.barros@gmail.com>
2025-06-09 20:52:54 -0300

    feat: insert controllersand test API request

---
+++ src/main/java/com/luizbarros/crud/controllers/ClientController.java
@@ -0,0 +1,15 @@
+package com.luizbarros.crud.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/clients")
+public class ClientController {
+	
+	@GetMapping
+	public String test() {
+		return "Test";		
+	}
+}
