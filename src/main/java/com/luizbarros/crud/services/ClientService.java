1bc8564e979f48568ba1f7b4bb1d500bc387f7e1
Luiz Barros <lck.barros@gmail.com>
2025-06-13 14:51:09 -0300

    feat: added pagination logic in ClientService

--- src/main/java/com/luizbarros/crud/services/ClientService.java
+++ src/main/java/com/luizbarros/crud/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.luizbarros.crud.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +20,11 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).toList();
 	}
 }
