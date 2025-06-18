b18c11d770d6078179d7d8709c31cd9e14a016bc
Luiz Barros <lck.barros@gmail.com>
2025-06-18 20:09:42 -0300

    fix: added serialVersionUID

--- src/main/java/com/luizbarros/crud/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/luizbarros/crud/services/exceptions/ResourceNotFoundException.java
@@ -2,6 +2,8 @@ package com.luizbarros.crud.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 	
+	private static final long serialVersionUID = 1L;
+
 	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}
