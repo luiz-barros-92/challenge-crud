51b194051b44ee7cf7d72a3040caca06f0e5c448
Luiz Barros <lck.barros@gmail.com>
2025-06-20 20:47:09 -0300

    feat: implement DataBaseException in ControllerExceptonHandler

--- src/main/java/com/luizbarros/crud/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/luizbarros/crud/controllers/handlers/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.luizbarros.crud.dto.CustomError;
+import com.luizbarros.crud.services.exceptions.DataBaseException;
 import com.luizbarros.crud.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,13 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity <CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<CustomError> database(DataBaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
